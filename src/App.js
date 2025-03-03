c4f7629112a7fb1661a380d69ff27da4b8a4a766
youngwoo-nam <silverfox32@naver.com>
2025-03-04 02:51:53 +0900

    react 버전 문제로 detail 페이지 안나옴

--- src/App.js
+++ src/App.js
@@ -1,52 +1,32 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route 
+} from "react-router-dom";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+
+function App(){
+
+  return <Router> 
+
+    <Navigation />
+    <Routes>
+     { <Route path="/" element={<Home />} > </Route> }
+     { <Route path="/about" element={<About />}></Route> }
+     { <Route path="/movie/:id" element={<Detail />}></Route> }
+  </Routes>
+</Router>;
+
+
+//    <Route path="/movie/:id" element={<About />}></Route>
+// { <Route path={process.env.PUBLIC_URL+"/"} element={<Home />} > </Route> }
+// { <Route path={process.env.PUBLIC_URL+"/about"} element={<About />}></Route> }
+// <Route path={"/"} element={<Home />} > </Route>
+// <Route path={"/about"} element={<About />}></Route>
 }
-
 export default App;
